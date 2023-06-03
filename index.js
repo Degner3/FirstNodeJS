e29ebc7dc3bde0a1ddf2b08e9a4d8a9335ae0bba
Degner3 <niklaschristiansen@live.dk>
2023-06-03 21:39:38 +0200

    Node

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+import http from 'http';
+
+
+// http.createServer((request, response) => {
+//     console.log('Hello world');
+// }).listen(4000)
+
+
+http.createServer((request, response) => {
+    // console.log('Server kører på http://localhost:4000');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('Hello World!');
+    response.end();
+}).listen(4000, () => {
+    console.log('Server kører på http://localhost:4000');
+})
